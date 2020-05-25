824a8bdc5d11f3d4508280d352f7596923cf1bb2
Tookiri <Blaze@RhodesIsland>
2020-05-25 14:55:39 +0800

    FifthTest

--- src/main/java/Custom.java
+++ src/main/java/Custom.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+public class Custom implements Person {
+    public void eat() {
+        System.out.println("来宾饭店就餐");
+    }
 
-public class Custom {
+    public void sleep() {
+        System.out.println("来宾宾馆休息");
+    }
 }
--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+public class Teacher implements Person {
+    public void eat() {
+        System.out.println("教师食堂就餐");
+    }
 
-public class Teacher {
+    public void sleep() {
+        System.out.println("教师宿舍休息");
+    }
 }
