9ac595b116df3fc0dfad0454a3fd8a19d263ece9
Tookiri <Blaze@RhodesIsland>
2020-05-25 14:18:27 +0800

    Develop_Test1

---
+++ src/main/java/Student.java
@@ -0,0 +1,9 @@
+public class Student implements Person {
+    public void eat() {
+        System.out.println("食堂就餐");
+    }
+
+    public void sleep() {
+        System.out.println("寝室休息");
+    }
+}
