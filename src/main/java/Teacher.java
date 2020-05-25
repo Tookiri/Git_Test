8f9aada88bee4569d089cb9bd36c5156988dd869
Tookiri <Blaze@RhodesIsland>
2020-05-25 14:27:09 +0800

    secondTest

---
+++ src/main/java/Teacher.java
@@ -0,0 +1,10 @@
+public class Teacher implements Person{
+
+    public void eat() {
+        System.out.println("教师食堂就餐");
+    }
+
+    public void sleep() {
+        System.out.println("教师公寓休息");
+    }
+}
